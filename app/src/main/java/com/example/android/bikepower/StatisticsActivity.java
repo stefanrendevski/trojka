0c3d6405fb460c2679def1089dbf104e0f9b729e
Dennis Nuhiji <dennisnuhiji@gmail.com>
2018-06-25 10:05:00 +0200

    read from json file and show on chart

--- app/src/main/java/com/example/android/bikepower/StatisticsActivity.java
+++ app/src/main/java/com/example/android/bikepower/StatisticsActivity.java
@@ -1,10 +1,46 @@
 package com.example.android.bikepower;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.LoaderManager;
+import android.content.CursorLoader;
+import android.content.Loader;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import static android.Manifest.permission.READ_CONTACTS;
+import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
@@ -16,14 +52,17 @@ import br.com.felix.horizontalbargraph.model.BarItem;
 
 public class StatisticsActivity extends AppCompatActivity {
 
+    private static final int REQUEST_READ_PERMISSION = 0;
+    private HorizontalBar horizontal;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
-
+        populate();
         ActionBar actionBar = this.getSupportActionBar();
 
-        HorizontalBar horizontal = findViewById(R.id.horizontal);
+        horizontal = findViewById(R.id.horizontal);
         if(horizontal!=null){
         }
         // Set the action bar back button to look like an up button
@@ -32,14 +71,56 @@ public class StatisticsActivity extends AppCompatActivity {
             horizontal.init(this).addAll(items()).build();
         }
     }
-    public List items(){
+    public void populate(){
+        if (!getPermmisions()) {
+            return;
+        }
+    }
+    public boolean getPermmisions(){
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            return true;
+        }
+        if (checkSelfPermission(READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+        if (shouldShowRequestPermissionRationale(READ_EXTERNAL_STORAGE)) {
+            Snackbar.make(horizontal, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
+                    .setAction(android.R.string.ok, new View.OnClickListener() {
+                        @Override
+                        @TargetApi(Build.VERSION_CODES.M)
+                        public void onClick(View v) {
+                            requestPermissions(new String[]{READ_EXTERNAL_STORAGE}, REQUEST_READ_PERMISSION);
+                        }
+                    });
+        } else {
+            requestPermissions(new String[]{READ_EXTERNAL_STORAGE}, REQUEST_READ_PERMISSION);
+        }
+        return false;
+    }
+    public List items() {
         List<BarItem> list=new ArrayList<>();
-        list.add(new BarItem("17/01/2018",105d));
-        list.add(new BarItem("18/01/2018",170d));
-        list.add(new BarItem("19/01/2018",200d));
-        list.add(new BarItem("20/01/2018",135d));
-        list.add(new BarItem("21/01/2018",100d));
-        list.add(new BarItem("22/01/2018",300d));
+        try {
+            File file = new File(Environment.getExternalStorageDirectory(), "statistics.json");
+            FileInputStream stream = new FileInputStream(file);
+            String jsonStr;
+            try {
+                FileChannel fc = stream.getChannel();
+                MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0, fc.size());
+                jsonStr = Charset.defaultCharset().decode(bb).toString();
+                JSONArray arr=new JSONArray(jsonStr);
+                for(int i=0;i<arr.length();i++){
+                    JSONObject jInside=arr.getJSONObject(i);
+                    String key=jInside.keys().next();
+                    list.add(new BarItem(key, Double.parseDouble(jInside.getString(key))));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }finally {
+                stream.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return list;
-    };
+    }
 }
